c7920c331e3d2b917eda0d847be9801c8d114bad
kinur28 <kikinrlaul@gmail.com>
2023-10-23 01:41:39 +0700

    update gallery

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -49,39 +49,16 @@
     <!-- gallery photos -->
     <section class="gallery-photos">
       <div class="content">
-        <div class="row-2">
-          <div class="image-pinned">
-            <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-0" src="https://www.instagram.com/p/CxH9xLDOITN/embed/?cr=1&amp;v=14&amp;wp=652&amp;rd=http%3A%2F%2F127.0.0.1%3A8000&amp;rp=%2Fgallery#%7B%22ci%22%3A0%2C%22os%22%3A336.6999999284744%2C%22ls%22%3A181.10000002384186%2C%22le%22%3A200.5%7D" allowtransparency="true" allowfullscreen="true" frameborder="0" height="390" data-instgrm-payload-id="instagram-media-payload-0" scrolling="no" style="background: white; max-width: 540px; width: calc(100% - 2px); border-radius: 3px; border: 1px solid rgb(219, 219, 219); box-shadow: none; display: block; margin: -55px 0px 12px; min-width: 550px; padding: 0px;"></iframe>
-            <script async src="//www.instagram.com/embed.js"></script>
-          </div>
-          <div class="image-pinned">
-            <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-1" src="https://www.instagram.com/p/CyIxE_fvL0v/embed/?cr=1&amp;v=14&amp;wp=652&amp;rd=http%3A%2F%2F127.0.0.1%3A8000&amp;rp=%2Fgallery#%7B%22ci%22%3A1%2C%22os%22%3A338.5%2C%22ls%22%3A181.10000002384186%2C%22le%22%3A200.5%7D" allowtransparency="true" allowfullscreen="true" frameborder="0" height="390" data-instgrm-payload-id="instagram-media-payload-1" scrolling="no" style="background: white; max-width: 540px; width: calc(100% - 2px); border-radius: 3px; border: 1px solid rgb(219, 219, 219); box-shadow: none; display: block; margin: -55px 0px 12px; min-width: 550px; padding: 0px;"></iframe>
-            <script async src="//www.instagram.com/embed.js"></script>
-          </div>
+        <div class="row-pinned">
+          <div class="image-pinned"><img src="img/instagram/post_01_08_2023_IstanaNegara.jpeg" alt="" /></div>
+          <div class="image-pinned"><img src="img/instagram/post_10_10_2023_BaliNusaDuaConventionCenter.jpeg" alt="" /></div>
         </div>
         
-        <div class="image">
-          <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-2" src="https://www.instagram.com/p/CygBPHCPciA/embed/?cr=1&amp;v=14&amp;wp=652&amp;rd=http%3A%2F%2F127.0.0.1%3A8000&amp;rp=%2Fgallery#%7B%22ci%22%3A2%2C%22os%22%3A339.7999999523163%2C%22ls%22%3A181.10000002384186%2C%22le%22%3A200.5%7D" allowtransparency="true" allowfullscreen="true" frameborder="0" height="390" data-instgrm-payload-id="instagram-media-payload-2" scrolling="no" style="background: white; max-width: 540px; width: calc(90% - 2px); border-radius: 3px; border: 1px solid rgb(219, 219, 219); box-shadow: none; display: block; margin: -55px 0px 12px; min-width: 265px; padding: 0px;"></iframe>
-          <script async src="//www.instagram.com/embed.js"></script>
-        </div>
-        <div class="image">
-          <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-3" src="https://www.instagram.com/p/CydT_JcP2Zq/embed/?cr=1&amp;v=14&amp;wp=652&amp;rd=http%3A%2F%2F127.0.0.1%3A8000&amp;rp=%2Fgallery#%7B%22ci%22%3A3%2C%22os%22%3A1297%2C%22ls%22%3A181.10000002384186%2C%22le%22%3A200.5%7D" allowtransparency="true" allowfullscreen="true" frameborder="0" height="390" data-instgrm-payload-id="instagram-media-payload-3" scrolling="no" style="background: white; max-width: 540px; width: calc(90% - 2px); border-radius: 3px; border: 1px solid rgb(219, 219, 219); box-shadow: none; display: block; margin: -55px 0px 12px; min-width: 265px; padding: 0px;"></iframe>
-          <script async src="//www.instagram.com/embed.js"></script>
-        </div>
-        <div class="image">
-          <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-4" src="https://www.instagram.com/p/CydRe3ovQRq/embed/?cr=1&amp;v=14&amp;wp=652&amp;rd=http%3A%2F%2F127.0.0.1%3A8000&amp;rp=%2Fgallery#%7B%22ci%22%3A4%2C%22os%22%3A1378.8999999761581%2C%22ls%22%3A181.10000002384186%2C%22le%22%3A200.5%7D" allowtransparency="true" allowfullscreen="true" frameborder="0" height="390" data-instgrm-payload-id="instagram-media-payload-4" scrolling="no" style="background: white; max-width: 540px; width: calc(90% - 2px); border-radius: 3px; border: 1px solid rgb(219, 219, 219); box-shadow: none; display: block; margin: -55px 0px 12px; min-width: 265px; padding: 0px;"></iframe>
-          <script async src="//www.instagram.com/embed.js"></script>
-        </div>
-        <div class="image">
-          <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-5" src="https://www.instagram.com/p/CyIxE_fvL0v/embed/?cr=1&amp;v=14&amp;wp=652&amp;rd=http%3A%2F%2F127.0.0.1%3A8000&amp;rp=%2Fgallery#%7B%22ci%22%3A5%2C%22os%22%3A1559.5%2C%22ls%22%3A181.10000002384186%2C%22le%22%3A200.5%7D" allowtransparency="true" allowfullscreen="true" frameborder="0" height="390" data-instgrm-payload-id="instagram-media-payload-5" scrolling="no" style="background: white; max-width: 540px; width: calc(90% - 2px); border-radius: 3px; border: 1px solid rgb(219, 219, 219); box-shadow: none; display: block; margin: -55px 0px 12px; min-width: 265px; padding: 0px;"></iframe>
-          <script async src="//www.instagram.com/embed.js"></script>
-        </div>
-        <!-- <div class="image"><img src="img/placeholder img.jpg" alt="" /></div>
-        <div class="image"><img src="img/placeholder img.jpg" alt="" /></div>
-        <div class="image"><img src="img/placeholder img.jpg" alt="" /></div>
-        <div class="image"><img src="img/placeholder img.jpg" alt="" /></div>
-        <div class="image"><img src="img/placeholder img.jpg" alt="" /></div>
-        <div class="image"><img src="img/placeholder img.jpg" alt="" /></div> -->
+
+        <div class="image"><img src="img/instagram/post_18_10_2023_BandaraKertajati.jpg" alt="" /></div>
+        <div class="image"><img src="img/instagram/post_15_10_2023_BandaraKertajati.jpg" alt="" /></div>
+        <div class="image"><img src="img/instagram/post_17-20_10_2023_TheWestinJakarta.jpg" alt="" /></div>
+        <div class="image"><img src="img/instagram/post_14_10_2023_PlenaryHallJCC.jpg" alt="" /></div>
       </div>
     </section>
     <!-- gallery photos -->
